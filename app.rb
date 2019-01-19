1ebfc5d285b3e11470d1a904b7df9e2f9911a90a
Jason Mei <jason.mei403@gmail.com>
2019-01-19 04:06:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @coach = params[:coach]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @power_forward = params[:pf]
+    @small_forward = params[:sf]
+    @center = params[:c]
+    
     erb :team
   end
 end
